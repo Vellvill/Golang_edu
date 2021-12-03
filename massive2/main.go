5db7d87f45313b676bf83140c5dbdec5e34827c8
Timofey <gerasimoff.timofeiigit config --global user.email gerasimoff.timofeii>
2021-12-03 17:22:53 +0300

    Updated

--- massive2/main.go
+++ massive2/main.go
@@ -1,1 +1,43 @@
-package massive2
+package main
+
+import "fmt"
+
+func main(){
+	x:=[]int{1,2,3,7,4,4,4,4,5}
+	y:=[]int{4,5,6}
+	fmt.Println(Interception(x,y))
+}
+
+func Interception(x, y []int) []int{
+	res := []int{}
+	if len(x) == 0 || len(y) == 0{
+		return res
+	}
+
+		for _, v:= range x {
+			for _, z := range y {
+				if v == z {
+					m := v
+					res = append(res, m)
+				}
+			}
+		}
+
+	var resNew[]int
+
+	for _, v := range res {
+		skip := false
+		for _, u := range resNew {
+			if v == u {
+				skip = true
+				break
+			}
+		}
+		if !skip {
+			resNew = append(resNew, v)
+		}
+	}
+
+	return resNew
+}
+
