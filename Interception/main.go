dfbcefcd1da0804562b4582e94cd341c3a41f461
Timofey <gerasimoff.timofeiigit config --global user.email gerasimoff.timofeii>
2021-12-03 15:42:34 +0300

    updated

--- Interception/main.go
+++ Interception/main.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-//func findNumber(a []int) int {
-
-}
-
-func sum(a []int) int{
+func sum(a []int) int {
 	m := 0
-	for _, v := range a{
-		m +=v
+	for _, v := range a {
+		m += v
 	}
 	return m
 }
 
-func main() {
-	a := []int{1,2,3}
-fmt.Println(sum(a))
+func createSubsequence(a int) []int {
+	massive := []int{}
+	for i := 1; i <= a; i++ {
+		massive = append(massive, i)
+	}
+	return massive
 }
 
-
-
-
-
-
+func main() {
+	a, c := 100, 100
+	cmassive := createSubsequence(c)
+	cmassive[9] = 0
+	fmt.Println(sum(createSubsequence(a)) - sum(cmassive))
+}
